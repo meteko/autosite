8c0c6688fc59919bf973c35017363bd8c98b3f44
Soren Malling <soren@meteko.dk>
2018-11-09 12:23:14 +0100

    [TASK] Add description and author information to extension metadata
    Fixes #1

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,20 +1,20 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
   'title' => 'Autosite',
-  'description' => '',
+  'description' => 'Automatically load TypoScript and PageTS to a pagetree, based on your "sites" configuration. Give your site the same identifier as a extension and configuration is automatically loaded.',
   'category' => 'site',
   'state' => 'stable',
   'uploadfolder' => 0,
   'createDirs' => '',
   'modify_tables' => '',
   'clearCacheOnLoad' => 0,
-  'author' => 'Meteko Aps',
+  'author' => 'Soren Malling',
   'author_email' => 'soren@meteko.dk',
   'author_company' => 'Meteko Aps',
   'version' => '1.0.0',
   'constraints' => [
     'depends' => [
-      'typo3' => '9.5.0-9.9.99',
+      'typo3' => '9.5.0-9.5.99',
     ],
     'conflicts' => [
     ],
