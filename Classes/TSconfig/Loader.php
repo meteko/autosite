f4ed8862bca2f9ea6b1db965fa1c9f1002049d2e
Soren Malling <soren@meteko.dk>
2018-11-16 11:23:16 +0100

    [TASK] Improve class doc block
    Fixes #4

--- Classes/TSconfig/Loader.php
+++ Classes/TSconfig/Loader.php
@@ -1,17 +1,13 @@
 <?php
 namespace Meteko\Autosite\TSconfig;
-/*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
+
+/**
+ * This file is part of the "autosite" Extension for TYPO3 CMS.
  *
  * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
+ * LICENSE file that was distributed with this source code.
  */
+
 use TYPO3\CMS\Core\Exception\SiteNotFoundException;
 use TYPO3\CMS\Core\Package\Exception\UnknownPackageException;
 use TYPO3\CMS\Core\Package\Package;
